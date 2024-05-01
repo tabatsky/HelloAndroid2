7541d1ae2aad29533c6da1acf4019f223cd38335
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-05-02 03:54:46 +1000

    Initial commit

---
+++ app/src/main/java/jatx/helloandroid2/MainActivity.kt
@@ -0,0 +1,17 @@
+package jatx.helloandroid2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jatx.helloandroid2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
